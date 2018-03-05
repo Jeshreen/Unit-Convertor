022d16cccd5b5093bdc0abc02d2ce3603c4041ae
Jeshreen Balraj <jeshreen18995@gmail.com>
2018-03-05 12:59:24 +0530

    Initial Commit

---
+++ Coursework1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Coursework1
+//
+//  Created by Jeshreen Balraj on 3/5/18.
+//  Copyright © 2018 Jeshreen Balraj. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Coursework1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Coursework1
+//
+//  Created by Jeshreen Balraj on 3/5/18.
+//  Copyright © 2018 Jeshreen Balraj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
